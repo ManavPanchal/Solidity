3f173d632de8ef7bda5d7375c7680a2af4397de6
Manav <manavpanchal@simformsolutions.com>
2023-03-29 12:08:54 +0530

    Solidity. Simple Bank contract

---
+++ SimpleBank (1).sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract SimpleBank{
+    mapping(address => uint) internal balance;
+    event transaction(address depositor, uint amount, string statement);
+
+    modifier balanceAvailabilty(uint amount){
+        uint userBalance = balance[msg.sender];
+        require(userBalance >= amount, "insufficient balance");
+        _;
+    }
+
+    function Deposit() payable public{
+        balance[msg.sender] += msg.value;
+        emit transaction(msg.sender, msg.value * 1 ether, "deposited");
+    }
+
+    function Balance() external view returns(uint){
+        return balance[msg.sender];
+    }
+
+    function Withdraw(uint amount) public balanceAvailabilty(amount){
+
+        address payable sender = payable(msg.sender);
+        balance[msg.sender] -= amount * 1 ether;
+        sender.transfer(amount * 1 ether);
+        
+        emit transaction(msg.sender, amount * 1 ether, "credited");
+    }
+}
