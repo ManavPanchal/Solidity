06f61ad0957c8e60b29fc297ba96c1e5cf34b285
Manav Panchal <89974541+ManavPanchal@users.noreply.github.com>
2023-03-30 19:39:57 +0530

    Update SimpleBank.sol

--- SimpleBank.sol
+++ SimpleBank.sol
@@ -13,7 +13,7 @@ contract SimpleBank{
 
     function Deposit() payable public{
     
-    	require(msg.value/1 ether >= 1, "make sure to deposit amount in ether");
+    	require(msg.value/1 ether > 0, "make sure to deposit amount in ether");
     	
         balance[msg.sender] += msg.value;
         emit transaction(msg.sender, msg.value * 1 ether, "deposited");
